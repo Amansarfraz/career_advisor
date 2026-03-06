b819dd5a578d008d62d32d3a820988b9667b460b
Amansarfraz <amansarfraz05@gmail.com>
2026-03-06 11:56:46 +0500

    first commit

--- lib/screens/career_assessment_screen.dart
+++ lib/screens/career_assessment_screen.dart
@@ -279,7 +279,7 @@ class _CareerAssessmentScreenState extends State<CareerAssessmentScreen> {
                       );
                     }),
 
-                    const SizedBox(height: 20),
+                    const SizedBox(height: 40),
 
                     /// NEXT BUTTON
                     Center(
