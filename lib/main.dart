53b2df5c215fef60049d7e94d4fd47a8d54535f9
Amansarfraz <amansarfraz05@gmail.com>
2026-03-07 16:09:50 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,67 @@
+// // import 'package:flutter/material.dart';
+// // import 'screens/splash_screen.dart';
+// // import 'screens/get_started_screen.dart';
+// // import 'screens/login_screen.dart';
+// // import 'screens/career_assessment_screen.dart';
+// // import 'screens/career_recommendation_screen.dart';
+// // import 'screens/career_details_screen.dart';
+
+// // void main() {
+// //   runApp(const MyApp());
+// // }
+
+// // class MyApp extends StatefulWidget {
+// //   const MyApp({super.key});
+
+// //   static _MyAppState? of(BuildContext context) =>
+// //       context.findAncestorStateOfType<_MyAppState>();
+
+// //   @override
+// //   State<MyApp> createState() => _MyAppState();
+// // }
+
+// // class _MyAppState extends State<MyApp> {
+// //   bool isDarkMode = false;
+
+// //   void toggleTheme(bool value) {
+// //     setState(() {
+// //       isDarkMode = value;
+// //     });
+// //   }
+
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return MaterialApp(
+// //       debugShowCheckedModeBanner: false,
+
+// //       theme: ThemeData(
+// //         brightness: Brightness.light,
+// //         primarySwatch: Colors.blue,
+// //       ),
+
+// //       darkTheme: ThemeData(brightness: Brightness.dark),
+
+// //       themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light,
+
+// //       initialRoute: '/',
+
+// //       routes: {
+// //         '/': (context) => const SplashScreen(),
+// //         '/get_started_screen': (context) => const GetStartedScreen(),
+// //         '/login_screen': (context) => const LoginScreen(),
+// //         '/career_assessment_screen': (context) =>
+// //             const CareerAssessmentScreen(),
+// //         '/career_recommendation_screen': (context) =>
+// //             const CareerAssessmentScreen(),
+// //         '/career_details_screen': (context) => const CareerRecommendationScreen(
+// //           matchPercentage: 85,
+// //           careerName: "Software Engineer",
+// //           shortExplanation: ".....",
+// //         ),
+// //       },
+// //     );
+// //   }
+// // }
 // import 'package:flutter/material.dart';
 // import 'screens/splash_screen.dart';
 // import 'screens/get_started_screen.dart';
@@ -5,6 +69,7 @@
 // import 'screens/career_assessment_screen.dart';
 // import 'screens/career_recommendation_screen.dart';
 // import 'screens/career_details_screen.dart';
+// import 'screens/setting_screen.dart';
 
 // void main() {
 //   runApp(const MyApp());
@@ -53,10 +118,16 @@
 //             const CareerAssessmentScreen(),
 //         '/career_recommendation_screen': (context) =>
 //             const CareerAssessmentScreen(),
+
 //         '/career_details_screen': (context) => const CareerRecommendationScreen(
 //           matchPercentage: 85,
 //           careerName: "Software Engineer",
 //           shortExplanation: ".....",
+//         ),
+
+//         '/setting_screen': (context) => SettingScreen(
+//           isDark: MyApp.of(context)!.isDarkMode,
+//           onToggle: MyApp.of(context)!.toggleTheme,
 //         ),
 //       },
 //     );
@@ -78,6 +149,7 @@ void main() {
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  // Access the state anywhere to toggle theme
   static _MyAppState? of(BuildContext context) =>
       context.findAncestorStateOfType<_MyAppState>();
 
@@ -104,7 +176,10 @@ class _MyAppState extends State<MyApp> {
         primarySwatch: Colors.blue,
       ),
 
-      darkTheme: ThemeData(brightness: Brightness.dark),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+      ),
 
       themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light,
 
@@ -117,18 +192,14 @@ class _MyAppState extends State<MyApp> {
         '/career_assessment_screen': (context) =>
             const CareerAssessmentScreen(),
         '/career_recommendation_screen': (context) =>
-            const CareerAssessmentScreen(),
-
-        '/career_details_screen': (context) => const CareerRecommendationScreen(
-          matchPercentage: 85,
-          careerName: "Software Engineer",
-          shortExplanation: ".....",
-        ),
-
-        '/setting_screen': (context) => SettingScreen(
-          isDark: MyApp.of(context)!.isDarkMode,
-          onToggle: MyApp.of(context)!.toggleTheme,
-        ),
+            const CareerRecommendationScreen(
+              matchPercentage: 85,
+              careerName: "Software Engineer",
+              shortExplanation: "Based on your answers this career suits you.",
+            ),
+        '/career_details_screen': (context) =>
+            const CareerDetailsScreen(careerName: "Software Engineer"),
+        '/setting_screen': (context) => const SettingScreen(),
       },
     );
   }
--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -1,52 +1,268 @@
+// import 'package:flutter/material.dart';
+// import 'career_details_screen.dart';
+// import '../main.dart';
+
+// class SettingScreen extends StatelessWidget {
+//   final bool isDark;
+//   final Function(bool) onToggle;
+
+//   SettingScreen({required this.isDark, required this.onToggle});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Color(0xff1E3A8A),
+//         title: Text("Setting"),
+//         leading: IconButton(
+//           icon: Icon(Icons.arrow_back),
+//           onPressed: () {
+//             Navigator.pushReplacement(
+//               context,
+//               MaterialPageRoute(
+//                 builder: (context) =>
+//                     const CareerDetailsScreen(careerName: 'Software Engineer'),
+//               ),
+//             );
+//           },
+//         ),
+//       ),
+
+//       /// 👇 open CareerAssessmentScreen
+//       body: Padding(
+//         padding: const EdgeInsets.all(16),
+//         child: Column(
+//           children: [
+//             /// DARK MODE
+//             Container(
+//               padding: EdgeInsets.all(14),
+//               decoration: BoxDecoration(
+//                 border: Border.all(color: Colors.purple),
+//                 borderRadius: BorderRadius.circular(12),
+//               ),
+//               child: Row(
+//                 children: [
+//                   Icon(Icons.dark_mode, color: Colors.blue),
+
+//                   SizedBox(width: 12),
+
+//                   Expanded(
+//                     child: Column(
+//                       crossAxisAlignment: CrossAxisAlignment.start,
+//                       children: [
+//                         Text(
+//                           "Dark Mode",
+//                           style: TextStyle(fontWeight: FontWeight.bold),
+//                         ),
+//                         Text(
+//                           "Switch to dark mode",
+//                           style: TextStyle(fontSize: 12),
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+
+//                   Switch(
+//                     value: MyApp.of(context)!.isDarkMode,
+//                     onChanged: (value) {
+//                       MyApp.of(context)!.toggleTheme(value);
+//                     },
+//                   ), // Switch(value: isDark, onChanged: onToggle),
+//                 ],
+//               ),
+//             ),
+
+//             SizedBox(height: 15),
+
+//             /// ABOUT APP
+//             GestureDetector(
+//               onTap: () {
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(builder: (context) => AboutScreen()),
+//                 );
+//               },
+
+//               child: Container(
+//                 padding: EdgeInsets.all(14),
+//                 decoration: BoxDecoration(
+//                   border: Border.all(color: Colors.purple),
+//                   borderRadius: BorderRadius.circular(12),
+//                 ),
+
+//                 child: Row(
+//                   children: [
+//                     Icon(Icons.info, color: Color(0xff1E3A8A)),
+
+//                     SizedBox(width: 12),
+
+//                     Expanded(
+//                       child: Column(
+//                         crossAxisAlignment: CrossAxisAlignment.start,
+//                         children: [
+//                           Text(
+//                             "About App",
+//                             style: TextStyle(fontWeight: FontWeight.bold),
+//                           ),
+//                           Text(
+//                             "Developer information",
+//                             style: TextStyle(fontSize: 12),
+//                           ),
+//                         ],
+//                       ),
+//                     ),
+
+//                     Icon(Icons.arrow_forward_ios, size: 16),
+//                   ],
+//                 ),
+//               ),
+//             ),
+
+//             SizedBox(height: 15),
+
+//             /// LOGOUT
+//             GestureDetector(
+//               onTap: () {
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(builder: (context) => LogoutScreen()),
+//                 );
+//               },
+
+//               child: Container(
+//                 padding: EdgeInsets.all(14),
+//                 decoration: BoxDecoration(
+//                   border: Border.all(color: Colors.red),
+//                   borderRadius: BorderRadius.circular(12),
+//                 ),
+
+//                 child: Row(
+//                   children: [
+//                     Icon(Icons.logout, color: Colors.red),
+
+//                     SizedBox(width: 12),
+
+//                     Expanded(
+//                       child: Column(
+//                         crossAxisAlignment: CrossAxisAlignment.start,
+//                         children: [
+//                           Text(
+//                             "Logout",
+//                             style: TextStyle(
+//                               fontWeight: FontWeight.bold,
+//                               color: Colors.red,
+//                             ),
+//                           ),
+//                           Text(
+//                             "Sign out of your account",
+//                             style: TextStyle(fontSize: 12),
+//                           ),
+//                         ],
+//                       ),
+//                     ),
+
+//                     Icon(Icons.arrow_forward_ios, size: 16),
+//                   ],
+//                 ),
+//               ),
+//             ),
+
+//             SizedBox(height: 40),
+
+//             /// BLUE CIRCLE
+//             Container(
+//               height: 120,
+//               width: 120,
+//               decoration: BoxDecoration(
+//                 shape: BoxShape.circle,
+//                 color: Color(0xff1E3A8A),
+//               ),
+
+//               child: Padding(
+//                 padding: const EdgeInsets.all(25),
+//                 child: Image.asset("assets/images/logo.png"),
+//               ),
+//             ),
+
+//             SizedBox(height: 10),
+
+//             /// TAGLINE
+//             Text(
+//               "Career Advisor",
+//               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+//             ),
+
+//             SizedBox(height: 5),
+
+//             Text(
+//               "Guiding Your Future, Step by Step",
+//               textAlign: TextAlign.center,
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class AboutScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text("About App")),
+//       body: Center(child: Text("Developer Info")),
+//     );
+//   }
+// }
+
+// class LogoutScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text("Logout")),
+//       body: Center(child: Text("Logout Screen")),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'career_details_screen.dart';
 import '../main.dart';
 
 class SettingScreen extends StatelessWidget {
-  final bool isDark;
-  final Function(bool) onToggle;
-
-  SettingScreen({required this.isDark, required this.onToggle});
+  const SettingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final appState = MyApp.of(context)!; // Access global dark mode
+
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xff1E3A8A),
-        title: Text("Setting"),
+        backgroundColor: const Color(0xff1E3A8A),
+        title: const Text("Settings"),
         leading: IconButton(
-          icon: Icon(Icons.arrow_back),
+          icon: const Icon(Icons.arrow_back),
           onPressed: () {
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(
-                builder: (context) =>
-                    const CareerDetailsScreen(careerName: 'Software Engineer'),
-              ),
-            );
+            Navigator.pop(context); // Simply go back to previous screen
           },
         ),
       ),
-
-      /// 👇 open CareerAssessmentScreen
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
           children: [
             /// DARK MODE
             Container(
-              padding: EdgeInsets.all(14),
+              padding: const EdgeInsets.all(14),
               decoration: BoxDecoration(
                 border: Border.all(color: Colors.purple),
                 borderRadius: BorderRadius.circular(12),
               ),
               child: Row(
                 children: [
-                  Icon(Icons.dark_mode, color: Colors.blue),
-
-                  SizedBox(width: 12),
-
-                  Expanded(
+                  const Icon(Icons.dark_mode, color: Colors.blue),
+                  const SizedBox(width: 12),
+                  const Expanded(
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
@@ -61,41 +277,34 @@ class SettingScreen extends StatelessWidget {
                       ],
                     ),
                   ),
-
                   Switch(
-                    value: MyApp.of(context)!.isDarkMode,
-                    onChanged: (value) {
-                      MyApp.of(context)!.toggleTheme(value);
-                    },
-                  ), // Switch(value: isDark, onChanged: onToggle),
+                    value: appState.isDarkMode,
+                    onChanged: (value) => appState.toggleTheme(value),
+                  ),
                 ],
               ),
             ),
 
-            SizedBox(height: 15),
+            const SizedBox(height: 15),
 
             /// ABOUT APP
             GestureDetector(
               onTap: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => AboutScreen()),
+                  MaterialPageRoute(builder: (context) => const AboutScreen()),
                 );
               },
-
               child: Container(
-                padding: EdgeInsets.all(14),
+                padding: const EdgeInsets.all(14),
                 decoration: BoxDecoration(
                   border: Border.all(color: Colors.purple),
                   borderRadius: BorderRadius.circular(12),
                 ),
-
                 child: Row(
-                  children: [
+                  children: const [
                     Icon(Icons.info, color: Color(0xff1E3A8A)),
-
                     SizedBox(width: 12),
-
                     Expanded(
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
@@ -111,37 +320,32 @@ class SettingScreen extends StatelessWidget {
                         ],
                       ),
                     ),
-
                     Icon(Icons.arrow_forward_ios, size: 16),
                   ],
                 ),
               ),
             ),
 
-            SizedBox(height: 15),
+            const SizedBox(height: 15),
 
             /// LOGOUT
             GestureDetector(
               onTap: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => LogoutScreen()),
+                  MaterialPageRoute(builder: (context) => const LogoutScreen()),
                 );
               },
-
               child: Container(
-                padding: EdgeInsets.all(14),
+                padding: const EdgeInsets.all(14),
                 decoration: BoxDecoration(
                   border: Border.all(color: Colors.red),
                   borderRadius: BorderRadius.circular(12),
                 ),
-
                 child: Row(
-                  children: [
+                  children: const [
                     Icon(Icons.logout, color: Colors.red),
-
                     SizedBox(width: 12),
-
                     Expanded(
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
@@ -160,41 +364,39 @@ class SettingScreen extends StatelessWidget {
                         ],
                       ),
                     ),
-
                     Icon(Icons.arrow_forward_ios, size: 16),
                   ],
                 ),
               ),
             ),
 
-            SizedBox(height: 40),
+            const SizedBox(height: 40),
 
-            /// BLUE CIRCLE
+            /// BLUE CIRCLE LOGO
             Container(
               height: 120,
               width: 120,
-              decoration: BoxDecoration(
+              decoration: const BoxDecoration(
                 shape: BoxShape.circle,
                 color: Color(0xff1E3A8A),
               ),
-
               child: Padding(
-                padding: const EdgeInsets.all(25),
+                padding: const EdgeInsets.all(30),
                 child: Image.asset("assets/images/logo.png"),
               ),
             ),
 
-            SizedBox(height: 10),
+            const SizedBox(height: 10),
 
             /// TAGLINE
-            Text(
+            const Text(
               "Career Advisor",
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
             ),
 
-            SizedBox(height: 5),
+            const SizedBox(height: 5),
 
-            Text(
+            const Text(
               "Guiding Your Future, Step by Step",
               textAlign: TextAlign.center,
             ),
@@ -206,21 +408,25 @@ class SettingScreen extends StatelessWidget {
 }
 
 class AboutScreen extends StatelessWidget {
+  const AboutScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("About App")),
-      body: Center(child: Text("Developer Info")),
+      appBar: AppBar(title: const Text("About App")),
+      body: const Center(child: Text("Developer Info")),
     );
   }
 }
 
 class LogoutScreen extends StatelessWidget {
+  const LogoutScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Logout")),
-      body: Center(child: Text("Logout Screen")),
+      appBar: AppBar(title: const Text("Logout")),
+      body: const Center(child: Text("Logout Screen")),
     );
   }
 }
