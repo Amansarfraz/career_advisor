2ce185574c24c2553df9777dc63926a0551fa474
Amansarfraz <amansarfraz05@gmail.com>
2026-03-15 22:13:18 +0500

    first commit

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,68 +1,68 @@
-import 'dart:async';
-import 'package:career_advisor/screens/get_started_screen.dart';
-import 'package:flutter/material.dart';
+// import 'dart:async';
+// import 'package:career_advisor/screens/get_started_screen.dart';
+// import 'package:flutter/material.dart';
 
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+// class SplashScreen extends StatefulWidget {
+//   const SplashScreen({super.key});
 
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
+//   @override
+//   State<SplashScreen> createState() => _SplashScreenState();
+// }
 
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
+// class _SplashScreenState extends State<SplashScreen> {
+//   @override
+//   void initState() {
+//     super.initState();
 
-    Timer(const Duration(seconds: 3), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const GetStartedScreen()),
-      );
-    });
-  }
+//     Timer(const Duration(seconds: 3), () {
+//       Navigator.pushReplacement(
+//         context,
+//         MaterialPageRoute(builder: (context) => const GetStartedScreen()),
+//       );
+//     });
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         width: double.infinity,
+//         height: double.infinity,
 
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Color(0xFF1E3A8A), Color(0xFF3B82F6)],
-          ),
-        ),
+//         decoration: const BoxDecoration(
+//           gradient: LinearGradient(
+//             begin: Alignment.topCenter,
+//             end: Alignment.bottomCenter,
+//             colors: [Color(0xFF1E3A8A), Color(0xFF3B82F6)],
+//           ),
+//         ),
 
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            /// LOGO
-            Image.asset(
-              'assets/images/logo.png',
-              width: 350,
-              height: 350,
-              fit: BoxFit.cover,
-            ),
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           children: [
+//             /// LOGO
+//             Image.asset(
+//               'assets/images/logo.png',
+//               width: 350,
+//               height: 350,
+//               fit: BoxFit.cover,
+//             ),
 
-            /// TAGLINE (logo ke foran next line)
-            const Text(
-              'Guiding Your Future, Step\nby Step',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontFamily: 'Poppins',
-                fontSize: 30,
-                fontWeight: FontWeight.w400,
-                color: Colors.white,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+//             /// TAGLINE (logo ke foran next line)
+//             const Text(
+//               'Guiding Your Future, Step\nby Step',
+//               textAlign: TextAlign.center,
+//               style: TextStyle(
+//                 fontFamily: 'Poppins',
+//                 fontSize: 30,
+//                 fontWeight: FontWeight.w400,
+//                 color: Colors.white,
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
