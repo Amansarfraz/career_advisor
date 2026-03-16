126f56fc8cd300640528b1ab3c47ae3a02e920cd
Amansarfraz <amansarfraz05@gmail.com>
2026-03-16 12:55:09 +0500

    first commit

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,15 @@
+# from pymongo import MongoClient
+# from config import MONGO_URL, DATABASE_NAME
+
+# client = MongoClient(MONGO_URL)
+
+# db = client("mongodb://localhost:27017")["career_advisor_db"]
+
+# users_collection = db["users"]
+# assessment_collection = db["assessments"]
 from pymongo import MongoClient
-from config import MONGO_URL, DATABASE_NAME
 
-client = MongoClient(MONGO_URL)
-
-db = client["career_advisor_db"]
-
+client = MongoClient("mongodb://localhost:27017")  # ✅ correct
+db = client["career_advisor_db"]                   # ✅ correct
 users_collection = db["users"]
 assessment_collection = db["assessments"]
